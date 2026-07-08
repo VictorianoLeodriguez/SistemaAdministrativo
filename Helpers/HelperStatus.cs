3edf18803c78cbffff1daca4f6a91a2cce73c7af
Victoriano <ev_leodriguez@hotmail.com>
2026-07-08 17:44:34 -0300

    Adição de Olhar senha e mais algumas

---
+++ Helpers/HelperStatus.cs
@@ -0,0 +1,18 @@
+public enum CargoSystem{
+    Admin = 0,
+    Client = 1,
+}
+
+public enum CargoStatusClient
+{
+    Ativo = 0, 
+    inativo = 1
+}
+
+public enum CargoInterno
+{
+    RH = 0,
+    Contabilidade = 1,
+    Administrativo = 2,
+    Fiscal = 3
+}
